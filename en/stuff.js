611952ae0568e8db5a8276a318810cc2130dc876
Prabhat Kumar <prabhat.kumar@sourcebits.com>
2014-12-19 17:15:22 +0530

    Update stuff.js

--- en/stuff.js
+++ en/stuff.js
@@ -2,5 +2,6 @@ define(['jquery'], function ($) {
     // do some awesome css stuff here
     $(document).ready(function(){
       $("body").css("background","black");
+      $("div").css({"text-align":"center","color":"white"}).text("Prabhat Kumar");
     });
 });
